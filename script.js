07bbc6d0a67e1801423fcc232ec04be6651ab2a1
rizqizulfian <rizqizulfian@gmail.com>
2021-02-25 18:25:44 +0700

    Add database todo

---
+++ script.js
@@ -0,0 +1,34 @@
+// Data input
+let data = [
+    {
+        title: 'Buat HTML',
+        category: 'todoList',
+        tanggal: '01-12-2020'
+    },
+    {
+        title: 'Design UI',
+        category: 'todoList',
+        tanggal: '30-12-2020'
+    },
+    {
+        title: 'Buat JS',
+        category: 'onProgress',
+        tanggal: '01-02-2020'
+    },
+    {
+        title: 'Testing CSS',
+        category: 'test',
+        tanggal: '11-03-2020'
+    },
+    {
+        title: 'Pilih Tema Project',
+        category: 'done',
+        tanggal: '21-12-2020'
+    },
+    {
+        title: 'Bagi Tugas',
+        category: 'done',
+        tanggal: '23-12-2020'
+    },
+];
+
